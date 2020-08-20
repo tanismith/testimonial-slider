08b9317cf78a1eb1cccc225c8b05ec27e0f5359e
tanismith <taniasmith.design@gmail.com>
2020-08-20 18:14:33 -0500

    responsive

---
+++ main.js
@@ -0,0 +1,36 @@
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let index = 0;
+
+prev.addEventListener("click", function () {
+  prevSlide();
+});
+next.addEventListener("click", function () {
+  nextSlide();
+});
+
+function prevSlide() {
+  if (index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  changeSlide();
+}
+
+function nextSlide() {
+  if (index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeSlide();
+}
+
+function changeSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+}
